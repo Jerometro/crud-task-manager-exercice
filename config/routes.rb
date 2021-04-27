14bf88a91710708dc7f1de2b8d909cc2d8fc18fd
Jerome Trombetta <jrmtbta@gmail.com>
2021-04-27 19:44:28 +0200

    task manager exercice completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get '/tasks', to: 'tasks#index'
+  # post '/tasks', to: 'tasks#create'
+
+  # get '/tasks/new', to: 'tasks#new', as: :new_task
+  # get '/tasks/:id', to: 'tasks#show', as: :task
+  # delete '/tasks/:id', to: 'tasks#destroy'
+  # patch '/tasks/:id', to: 'tasks#update'
+
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  resources :tasks
 end
